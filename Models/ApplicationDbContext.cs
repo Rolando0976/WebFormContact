dec98b52762947e4af0305faa96bc10d69e05f06
jamz1328 <jamz132842@gmail.com>
2025-02-03 20:12:56 -0600

    Actualizacion de builder y de ApplicationDbContext

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -1,6 +1,12 @@
-﻿namespace WebFormContact.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebFormContact.Models
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+       : base(options) { }
+
+        public DbSet<ContactMessage> ContactMessages { get; set; }
     }
 }
